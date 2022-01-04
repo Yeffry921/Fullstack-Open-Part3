481961917d4e8dcc365e16f45d68b9596288d831
yeffry <yeffry45@gmail.com>
2022-01-04 13:29:27 -0500

    fix missing environment variable PORT

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
   console.log(`Server is up in port ${PORT}`)
