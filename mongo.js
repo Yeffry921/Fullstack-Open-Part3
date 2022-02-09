878ee52bc99fa089ab7022a3a6a3cc6839b4de10
yeffry <yeffry45@gmail.com>
2022-02-09 15:14:48 -0500

    extract database connection + schema to seperate file

--- mongo.js
+++ mongo.js
@@ -1,46 +1,20 @@
-// Connect to DB
 const mongoose = require('mongoose')
 
-if(process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
-}
-
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
-
-const url = `mongodb+srv://yeffry921:Naruto921@phones.a7zze.mongodb.net/Phonebook?retryWrites=true&w=majority`
+const url = process.env.MONGO_URI
 
 mongoose.connect(url)
 
-// Set up Schema
-const phoneSchema = new mongoose.Schema({
-  id: Number,
+const personSchema = new mongoose.Schema({
   name: String,
   number: Number,
 })
 
-const Phone = mongoose.model('Phone', phoneSchema)
-
-// Save data to DB
-
-const phone = new Phone({
-  name: name,
-  number: number,
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-if(!name & !number) {
-  Phone
-    .find({})
-    .then((result) => {
-      result.forEach((phone) => {
-      console.log(phone)
-      })
-      mongoose.connection.close()
-    })
-}
-
-phone.save().then((result) => {
-  console.log(console.log(`added ${result.name} number ${result.number} to phonebook`))
-})
+module.exports = mongoose.model('Person', personSchema)
